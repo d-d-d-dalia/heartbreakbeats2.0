f7e376360ffae2fc618e8c2531193688be1a8111
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2017-12-15 15:52:30 -0500

    still trying to fix scope for song_vibe

--- app/controllers/song_vibes_controller.rb
+++ app/controllers/song_vibes_controller.rb
@@ -3,10 +3,7 @@ class SongVibesController < ApplicationController
 	def most_severe_vibes
 		@user = current_user
 		@most_severe_vibes = SongVibe.highest_level(current_user.id)
-
-		@most_severe_vibes.map do |v|
-			v.vibe
-		end.uniq
+		# @songs = Song.all(current_user.id)
 	end
 
 end
--- app/models/song_vibe.rb
+++ app/models/song_vibe.rb
@@ -27,4 +27,13 @@ class SongVibe < ApplicationRecord
     end
   end
 
+  # def self.most_severe_vibes
+  #   @user = current_user
+  #   @most_severe_vibes = SongVibe.highest_level(current_user.id)
+
+  #   @most_severe_vibes.map do |v|
+  #     v.vibe
+  #   end.uniq
+  # end
+
 end
