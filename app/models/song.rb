2f6bb3af6456b6ea951dbe1fc9eb7341472b9fc2
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2017-12-18 17:45:51 -0500

    edit scratch file

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,8 @@ class Song < ApplicationRecord
   	has_many :vibes, through: :song_vibes
 
   	belongs_to :user
-
+  	
+  	# do i want song name to be unique within scope of user?
   	validates :name, :artist, :song_vibes, presence: true
 
   	accepts_nested_attributes_for :song_vibes, reject_if: proc { |attributes| attributes['level'].blank? }
