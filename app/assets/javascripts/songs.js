c3fc6cce653089129a7cb5115f86c8e010eca484
Dalia <daliasawaya@tests-air-2.attlocal.net>
2017-12-23 17:36:59 -0800

    add script with global variable to song page show to use in js next song function

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,11 +1,12 @@
 $(document).on('turbolinks:load', function() {
     listSongs()
     nextSong()
-    previousSong()
-    loadSongIndexes()
+    // previousSong()
+    // loadSongIndexes()
   })
 
 // Cat index on User home
+// the second argument of .getJSON is the function you call on the result of the query to the endpoint (in this case /songs)
 function listSongs () {
   $.getJSON("/songs", function(songs){
     $.each(songs, function(index, song) {
@@ -16,9 +17,9 @@ function listSongs () {
 }
 
 function nextSong () {
-  $.getJSON("/songs/:id/next", function(next_song) {
-    $("#song_name").replaceWith(next_song.name)
+  $("#link_to_next").click(function () {
+    $.getJSON(`/songs/${current_song_id}/next`, function(next_song) {
+      $("#song_name").replaceWith(next_song.name)
+    })
   })
 }
-
-$("#link_to_next").click (nextSong)
